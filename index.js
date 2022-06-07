b9679cfdbce00e65aba0273ecad497f9abaf65c6
Adam Nyberg <adamnyberg95@gmail.com>
2022-06-07 12:30:04 +0200

    change background

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-asdsadasdas;
+const box = document.getElementsByClassName("box");
+
+box.addEventListener("click", function onClick(event) {
+  // 👇️ change background color
+  document.body.style.backgroundColor = "black";
+});
